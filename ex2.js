779390ef2d5fcc94878a279da2f97f77bd2def28
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-10-01 09:43:09 +0100

    link added

--- ex2.js
+++ ex2.js
@@ -1,4 +1,5 @@
 //redefine these - ex1 should output the solution to project Euler Q1
+//https://projecteuler.net/problem=1
 //ex2 should output the sum of all multiples of a or b less than n
 
 let euler1=()=>alert();
