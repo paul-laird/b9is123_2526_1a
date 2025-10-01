f6c5c965041520473d22144c12360b7ab2eb2423
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-10-01 11:03:10 +0100

    Exercises involving lists

---
+++ ex3.js
@@ -0,0 +1,24 @@
+let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
+let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+
+let eulerlist=()=>
+{
+    a=2//can un-hardcode
+    b=3
+    l=[1,2,3,4,5,6,7,9,10,10,10]
+    //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
+}
+let euler2Lists=()=>
+{
+    a=[2,3]//can un-hardcode
+    l=[1,2,3,4,5,6,7,9,10,10,10]
+    //call listEuler2 and alert.
+}
+
+let euler2Lists1=()=>
+{
+    a=[2,3,5]//can un-hardcode
+    l=[1,2,3,4,5,6,7,9,10,10,10]
+    //call listEuler3 and alert.
+}
