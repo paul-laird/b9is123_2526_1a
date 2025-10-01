b95bf711832dcaf7a46fa3fad4e2e364d95f3747
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-10-01 09:33:16 +0100

    exercise 2

---
+++ ex2.js
@@ -0,0 +1,6 @@
+//redefine these - ex1 should output the solution to project Euler Q1
+//ex2 should output the sum of all multiples of a or b less than n
+
+let euler1=()=>alert();
+
+let eulerCustom=()=>alert()
