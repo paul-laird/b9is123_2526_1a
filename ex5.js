a4f98049c43ae170145082549687b954febd241f
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-10-07 14:25:17 +0100

    Ex 5 and 5a

---
+++ ex5.js
@@ -0,0 +1,7 @@
+//Create a modified version of ex4 that prepends "corrupt" where an error occurs
+
+//Examples
+
+//Create a further version that will output the answer omitting corrupted values
+//or "corrupt" if it cannot be resolved
+
