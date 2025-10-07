7d56ba5c15d07329d08368253adeea1aad947a96
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-10-07 14:41:55 +0100

    NaN - safe parse

--- ex5.js
+++ ex5.js
@@ -1,10 +1,17 @@
+    let parse=(i)=>{
+      o=parseInt(i)
+      if (isNaN(o))
+        throw("NaN");
+      return o;
+    }
+    
 //Create a modified version of ex4 that prepends "corrupt" where an error occurs
 
 //Examples ()
 // 2 3 5 67
 // corrupt : 2 3 5 67
 
-//Actually this one will not result in an error unless you throw one:
+//Actually this one will not result in an error unless you throw one as in parse(i) above:
 // 3 5 hello: 1 2 3 4 5 6 7 8 9
 // corrupt : 3 5 hello: 1 2 3 4 5 6 7 8 9
 
